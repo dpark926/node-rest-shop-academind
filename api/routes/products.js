fc85647c72581f23be727c8ce1e5751f05921173
Daniel Park <dpeace926@gmail.com>
2019-07-25 14:33:31 -0400

    mongoose validations, metadata

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,10 +6,25 @@ const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
   Product.find()
+    .select("name price _id")
     .exec()
     .then(docs => {
+      const response = {
+        count: docs.length,
+        products: docs.map(doc => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:5000/products/" + doc._id
+            }
+          };
+        })
+      };
       console.log(docs);
-      res.status(200).json(docs);
+      res.status(200).json(response);
 
       // if (docs.length >= 0) {
       //   res.status(200).json(docs);
@@ -37,8 +52,16 @@ router.post("/", (req, res, next) => {
     .then(result => {
       console.log(result);
       res.status(201).json({
-        message: "Handling POST requests to /products",
-        createdProduct: product
+        message: "Created product successfully",
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          _id: result._id,
+          request: {
+            type: "GET",
+            url: "http://localhost:5000/products" + result._id
+          }
+        }
       });
     })
     .catch(err => {
@@ -50,11 +73,18 @@ router.post("/", (req, res, next) => {
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
+    .select("name price _id")
     .exec()
     .then(doc => {
       console.log("From database", doc);
       if (doc) {
-        res.status(200).json(doc);
+        res.status(200).json({
+          product: doc,
+          request: {
+            type: "GET",
+            url: "http://localhost:5000/products/"
+          }
+        });
       } else {
         res
           .status(404)
@@ -88,9 +118,16 @@ router.patch("/:productId", (req, res, next) => {
     .exec()
     .then(result => {
       console.log(result);
-      res.status(200).json(result);
+      res.status(200).json({
+        message: "Product updated",
+        request: {
+          type: "GET",
+          url: "http://localhost:5000/product/" + id
+        }
+      });
     })
     .catch(err => {
+      console.log(err);
       res.status(500).json({ error: err });
     });
   // res.status(200).json({
@@ -103,7 +140,17 @@ router.delete("/:productId", (req, res, next) => {
   Product.remove({ _id: id })
     .exec()
     .then(result => {
-      res.status(200).json(result);
+      res.status(200).json({
+        message: "Product deleted",
+        request: {
+          type: "POST",
+          url: "http://localhost:5000/products",
+          body: {
+            name: "String",
+            price: "Number"
+          }
+        }
+      });
     })
     .catch(err => {
       console.log(err);
